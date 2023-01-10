9d0d5629269e55bef544f6681f253c31878f6fa4
webman <webmancity@gmail.com>
2023-01-10 13:40:51 +0300

    create users

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [invites, setInvites] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [searchValue, setSearchValue] = useState('')
+  const [success, setSuccess] = useState(false)
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json()).then(json => {
+        setUsers(json.data)
+      })
+      .catch(err => {
+        console.warn(err)
+        alert('Ошибка при получении пользователей')
+      }).finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value)
+  }
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id))
+    } else {
+      setInvites(prev => [...prev, id])
+    }
+  }
+
+  const onClickSendInvites = () => {
+    setSuccess(true)
+  }
+
   return (
-    <div>
-      Constructor React
+    <div className="App">
+      {success
+        ? <Success count={invites.length} />
+        : <Users
+          items={users}
+          isLoading={isLoading}
+          searchValue={searchValue}
+          onChangeSearchValue={onChangeSearchValue}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      }
     </div>
   );
 }
